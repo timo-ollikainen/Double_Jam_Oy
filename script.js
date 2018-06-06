6cf2f9c91e8cf04cf255fbf67f0bb121fb7bb3b5
timo-ollikainen <39961698+timo-ollikainen@users.noreply.github.com>
2018-06-06 22:07:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById('button');
+var hello = document.getElementById('hello');
+
+button.addEventListener('click', function () {
+		hello.innerHTML = 'What is your name? <input ';
+});
